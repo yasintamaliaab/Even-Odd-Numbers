7ec61db231ffe70438780f733c326c9e2ce98987
yasintamaliaab <31399708+yasintamaliaab@users.noreply.github.com>
2020-04-27 15:29:01 +0700

    First commit

---
+++ Test Logic NawaData 1/Nomor1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Nomor1
+{
+    public static void Main()
+    {
+        Console.Write("Masukkan Nilai Input : ");
+        string nilai = Console.ReadLine();
+        int n1 = Convert.ToInt32(nilai);
+        for (int i = 1; i <= n1; i++)
+        {
+            if (Odd(i))
+            {
+                Console.Write(i);
+            }
+        }
+        Console.Write(System.Environment.NewLine);
+        for (int j = 1; j <= n1; j++)
+        {
+            if (Even(j))
+            {
+                Console.Write(j);
+            }
+        }
+    }
+    public static bool Odd(int value)
+    {
+        return value % 2 != 0;
+    }
+    public static bool Even(int value)
+    {
+        return value % 2 == 0;
+    }
+}
